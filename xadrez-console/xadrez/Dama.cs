a15b1c45e416c0fec018d5598f9b2c198c04c54b
Eduardo Secolin <dusecolin@yahoo.com.br>
2017-11-19 12:40:52 -0200

    colocando peças no tabuleiro e criando classe das peças

---
+++ xadrez-console/xadrez/Dama.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using tabuleiro;
+
+namespace xadrez {
+    class Dama : Peca{
+        public Dama(Tabuleiro tab, Cor cor) : base(tab, cor) {
+        }
+        public override string ToString() {
+            return "D";
+        }
+    }
+}
---
+++ xadrez-console/xadrez/Rei.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using tabuleiro;
+
+namespace xadrez {
+    class Rei : Peca{
+        //construtor recebendo como parametro um tabuleiro e uma cor de peça referenciando a casse mãe = base
+        public Rei(Tabuleiro tab, Cor cor): base(tab, cor) {
+        }
+        //imprimmir a letra referente a peça de xadrez, nesse caso R = de "Rei"
+        public override string ToString() {
+            return "R";
+        }
+    }
+}
