49d52e2b8c46ac5f979922da0b56d6d349c466ca
Eduardo Secolin <dusecolin@yahoo.com.br>
2017-11-20 10:18:13 -0200

    Colocando cor nas pecas do tabuleiro

--- xadrez-console/Program.cs
+++ xadrez-console/Program.cs
@@ -9,20 +9,18 @@ using xadrez;
 namespace xadrez_console {
     class Program {
         static void Main(string[] args) {
-            /* try {
+             try {
                  Tabuleiro tab = new Tabuleiro(8, 8);
                  tab.colocarPeca(new Torre(tab, Cor.Preto), new Posicao(0, 0));
                  tab.colocarPeca(new Bispo(tab, Cor.Preto), new Posicao(1, 3));
                  tab.colocarPeca(new Rei(tab, Cor.Preto), new Posicao(7, 5));
-                 Tela.imprimirTabuleiro(tab);
+                 tab.colocarPeca(new Peao(tab, Cor.Branco), new Posicao(5, 2));
+                Tela.imprimirTabuleiro(tab);
              }
              catch(TabuleiroException e) {
                  Console.WriteLine(e.Message);
-             }*/
-            PosicaoXadrez pos = new PosicaoXadrez('c', 7);
-            Console.WriteLine(pos);
-            Console.WriteLine(pos.toPosicao());
-            Console.ReadLine();
+             }
+            Console.ReadKey();
         }
     }
 }
--- xadrez-console/Tela.cs
+++ xadrez-console/Tela.cs
@@ -9,15 +9,32 @@ namespace xadrez_console {
     class Tela {
         public static void imprimirTabuleiro(Tabuleiro tab) {
             for(int i = 0; i < tab.linhas; i++) {
+                Console.Write(8 - i + " ");
                 for(int j = 0; j < tab.colunas; j++) {
                     if (tab.peca(i, j) == null) {
                         Console.Write("- ");
                     }
                     else {
-                        Console.Write(tab.peca(i, j) + " ");
+                        imprimirPecaColorida(tab.peca(i, j));
+                        Console.Write(" ");
                     }
                 }
                 Console.WriteLine();
+            }
+            Console.WriteLine("  a b c d e f g h ");
+        }
+
+        public static void imprimirPecaColorida(Peca peca) {
+            if(peca.cor == Cor.Branco) {
+                ConsoleColor aux1 = Console.ForegroundColor;
+                Console.ForegroundColor = ConsoleColor.White;
+                Console.Write(peca);
+                Console.ForegroundColor = aux1;
+            }else {
+                ConsoleColor aux = Console.ForegroundColor;
+                Console.ForegroundColor = ConsoleColor.DarkBlue;
+                Console.Write(peca);
+                Console.ForegroundColor = aux;
             }
         }
     }
